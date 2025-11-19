66563a55e11f03ffe90749c3a9554ab1bbe30334
LuG <lucas.gasparetto23@outlook.com>
2025-11-19 00:10:02 -0300

    Ajuste no Package

--- Back_End/src/main/java/com/example/demo/model/Usuario.java
+++ Back_End/src/main/java/com/example/demo/model/Usuario.java
@@ -1,4 +1,4 @@
-package com.exemplo.demo.model;
+package com.example.demo.model;
 
 import jakarta.persistence.*;
 import lombok.*;
--- Back_End/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ Back_End/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -1,8 +1,8 @@
-package com.exemplo.demo.repository;
+package com.example.demo.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.exemplo.demo.model.Usuario;
+import com.example.demo.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
